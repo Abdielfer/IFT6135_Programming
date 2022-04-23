58b98292814fba30d71f8ebde5e3c79aca60e8fb
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-04-23 13:22:16 -0400

    Cos Sim wel done

--- assignment3/q3_solution.py
+++ assignment3/q3_solution.py
@@ -149,9 +149,4 @@ class CosineSimilarity(Module):
         ref: https://medium.com/geekculture/cosine-similarity-and-cosine-distance-48eed889a5c4#:~:text=Save-,Machine%20Learning%20Fundamentals%3A%20Cosine%20Similarity%20and%20Cosine%20Distance,they%20are%20to%20each%20other.
            + combining with eps to avoid zero divition. 
         """
-        numerator = bdot(x1, x2)
-        x1_norm = torch.norm(x1,p = 2, dim = 1)
-        x2_norm = torch.norm(x2,p = 2, dim = 1)
-        eps = torch.ones_like(numerator)*self.eps
-        return torch.div(numerator,torch.max((x1_norm* x2_norm),eps)) 
-
+        return F.cosine_similarity(x1, x2)
